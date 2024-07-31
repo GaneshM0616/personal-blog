0ae6c648f4efa6606ea1f75081334cc9d43f0062
GaneshM0616 <loneman549@gmail.com>
2024-07-31 21:23:10 +0530

    small changes

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from '../conf/conf'
+import conf from '../conf/conf.js'
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
@@ -12,23 +12,45 @@ export class AuthService{
 
      async createAccount ({email, password, name}){
         try{
-
+            const userAccount = await this.account.create(ID.unique(),email, password, name )
+            if( userAccount){
+                return this.login({email, password})
+            }else{
+                return userAccount
+            }
         }catch (error){
-            throw row
+            throw error
         }
-
      }
-     async login(){
 
+     async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password)
+        } catch (error) {
+            throw error
+        }
      }
+
      async getCurrentUser(){
-
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser()::", error)
+        }
+        return null
      }
-     async logout(){}
+
+     async logout(){
+        try {
+            return this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout()::", error)
+        }
+     }
+
 }
 
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('<PROJECT_ID>');               // Your project ID
 
-const account = new Account(client);
+const authService = new AuthService()
+
+export default authService;
